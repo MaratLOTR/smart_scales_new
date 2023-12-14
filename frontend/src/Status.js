18c4122e2f24a17908bb8eb6b3727dadf1bfa0de
MaratLOTR <maratabdrahmanov@yandex.ru>
2023-12-14 07:40:22 +0500

    status_query

--- frontend/src/Status.js
+++ frontend/src/Status.js
@@ -2,26 +2,35 @@ import React, { useEffect, useState } from 'react';
 
 const Status = ({ userId }) => {
   const [healthPoint, setHealthPoint] = useState(null);
-  const [description, setDescription] = useState(null)
+  const [description, setDescription] = useState(null);
+
+  const fetchHealthData = async () => {
+    try {
+      const response = await fetch(`http://192.168.0.112:8080/users/pulse_pressure_temperature/?user_id=${userId}`);
+      const data = await response.json();
+
+      // Извлечение значения health_point из полученных данных
+      const { status: { health_point } } = data;
+      const { status: { description } } = data;
+
+      setHealthPoint(health_point);
+      setDescription(description);
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    }
+  };
+
   useEffect(() => {
-    const fetchHealthData = async () => {
-      try {
-        const response = await fetch(`http://192.168.0.112:8080/users/pulse_pressure_temperature/?user_id=${userId}`);
-        const data = await response.json();
-
-        // Извлечение значения health_point из полученных данных
-        const { status: { health_point } } = data;
-        const { status: { description } } = data;
-
-        setHealthPoint(health_point);
-        setDescription(description);
-      } catch (error) {
-        console.error('Ошибка при получении данных:', error);
-      }
-    };
-
-    // Вызов функции для загрузки данных
+    // Вызываем fetchHealthData сразу после монтирования компонента
     fetchHealthData();
+
+    // Устанавливаем интервал для выполнения запроса каждые 10 секунд
+    const intervalId = setInterval(() => {
+      fetchHealthData();
+    }, 10000);
+
+    // Очищаем интервал при размонтировании компонента
+    return () => clearInterval(intervalId);
   }, [userId]); // Пустой массив зависимостей означает, что эффект выполняется только после монтирования компонента
 
   let healthImage;
@@ -36,7 +45,7 @@ const Status = ({ userId }) => {
   return (
     <div>
       <h1>Общая оценка здоровья: {healthPoint} <img src={healthImage} alt="Green Circle" style={{ width: '40px', height: '40px', marginRight: '5px' }} /> </h1>
-      <h1>Описание состояние: {description} </h1>
+      <h1>Описание состояния: {description} </h1>
     </div>
   );
 };
